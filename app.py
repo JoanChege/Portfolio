af7ea59187e431148a1ad3f94e67f951f9eeddab
JoanChege <jncheg@gmail.com>
2024-09-07 01:28:01 +0300

    add routes

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def landing():
+    return render_template('landing.html')
 
 @app.route('/about')
 def about():
