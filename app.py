45e9a9d3eefd8d31d893fb034236a19a2c468a26
JoanChege <jncheg@gmail.com>
2024-09-07 15:05:41 +0300

    fix the server with flask run

--- app.py
+++ app.py
@@ -3,32 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def landing():
-    return render_template('landing.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/education')
-def education():
-    return render_template('education.html')
-
-@app.route('/skills')
-def skills():
-    return render_template('skills.html')
-
-@app.route('/experience')
-def experience():
-    return render_template('experience.html')
-
-@app.route('/projects')
-def projects():
-    return render_template('projects.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
